eaa6fbb0e2a30e01ee4ca736d534b2868d022b56
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-14 15:22:26 +0800

    delete: delete unnecessary functions updateList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,12 @@
-import {useState} from "react";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
 
 const TodoList = () => {
-    const [todoItemList, setTodoItemList] = useState([]);
-    const updateList = (newItem) => {
-        setTodoItemList(oldTodoItemList => [...oldTodoItemList, newItem]);
-    }
-
     return (
         <div>
             <h4>Todo List</h4>
             <TodoGroup />
-            <TodoGenerator updateList={updateList} />
+            <TodoGenerator />
         </div>
     );
 }
