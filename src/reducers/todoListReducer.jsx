89231f2b034fa10cd8e54cd490aa5edc45cd143c
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-14 15:15:50 +0800

    feat: add action "updateList"

--- src/reducers/todoListReducer.jsx
+++ src/reducers/todoListReducer.jsx
@@ -1,5 +1,7 @@
 const todoListReducer = (state = {todoItemList: []}, action) => {
     switch (action.type) {
+        case "updateList":
+            return {...state, todoItemList: [...state.todoItemList, action.payload]}
         default:
             return state;
     }
